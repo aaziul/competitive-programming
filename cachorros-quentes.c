a258a8584f6800238b512c195eaa94e1b1309724
luiza <97320223+aaziul@users.noreply.github.com>
2024-10-05 15:07:48 -0300

    Create cachorros-quentes.c

---
+++ cachorros-quentes.c
@@ -0,0 +1,21 @@
+// BEECROWD: 2234
+// Em 2012 foi alcançado um novo recorde mundial na famosa Competição de Cachorros-Quentes do Nathan: o campeão, Joey Chestnut, devorou 68 cachorros-quentes em dez minutos, um aumento incrível em relação aos 62 sanduíches devorados pelo mesmo Chestnut em 2011.
+// O restaurante Nathan’s Famous Corporation, localizado no Brooklyn, NY, é o responsável pela competição. Eles produzem deliciosos cachorros-quentes, mundialmente famosos, mas quando o assunto é matemática eles não são tão bons. Eles desejam ser listados no Livro de Recordes do Guinness, mas para isso devem preencher um formulário descrevendo os fatos básicos da competição. Em particular, eles devem informar o número médio de cachorros-quentes consumidos pelos participantes durante a competição.
+// Você pode ajudá-los? Eles prometeram pagá-lo com um dos seus saborosos cachorros-quentes. Dados o número total de cachorros-quentes consumidos e o número total de participantes na competição, você deve escrever um programa para determinar o número médio de cachorros-quentes consumidos pelos participantes.
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	float H = 0, P = 0;
+	float media = 0;
+
+	scanf("%f", &H);
+	scanf("%f", &P);
+
+    media = H / P;
+
+	printf("%.2f\n", media);
+
+	return 0;
+}
